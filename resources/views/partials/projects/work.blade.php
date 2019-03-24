adefbc94830d0742ed49b9a1770d69667f97efa1
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 05:17:12 -0400

    projects

--- resources/views/partials/projects/work.blade.php
+++ resources/views/partials/projects/work.blade.php
@@ -27,6 +27,31 @@
     
              
             </div>
+
+            <div class="row mb-100">
+                    <!--/-/-/-/-/-/-/-/-/ 
+                            Begin Single Banner  
+                        -/-/-/-/-/-/-/-/-/-->
+                        <div class="col-sm-6 img-banner-r skewl">
+                       
+                               
+                                
+                                <div class="img-banner-content-r">
+              
+                                <a href="#">
+                              
+                                    <img class="banner-main-img" src="{{ asset('storage/assets/imgs/no-img.jpg')}}" alt="product-banner">
+                                </a>
+            
+                            
+                            </div>
+                        </div>
+                    <!--/-/-/-/-/-/-/-/-/ 
+                            End Single Banner  
+                        -/-/-/-/-/-/-/-/-/-->
+        
+                 
+                </div>
             <!--/-/-/-/-/-/-/-/-/ 
                     End Row 
                 -/-/-/-/-/-/-/-/-/-->
