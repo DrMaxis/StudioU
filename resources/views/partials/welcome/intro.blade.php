67cada90796c90155f222c8ad2b8e85875c8b70e
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 00:22:44 -0400

    .

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -17,16 +17,16 @@
                     </h1>
 
 
-                    <img style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
+                    <img class="overlay" style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
 
-                    <input type="checkbox" id="switch" checked>
-<label for="switch" class="switch-label">Turn </label>
 
 <div class="tvContainer">
   <iframe src="https://codepen.io" class="screen" width="800" height="600"></iframe>
   <div class="overlay">AV-1</div>
 </div>
                 
+                    <input type="checkbox" id="switch" checked>
+<label for="switch" class="switch-label">Turn </label>
                 </div>
 
 
