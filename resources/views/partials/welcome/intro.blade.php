0ccb4fdfb1a8170ecfa02d643c8a0f2dd870c7e2
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 04:28:33 -0400

    Update intro.blade.php

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -12,26 +12,40 @@
                 <div class="intro-secton" {{-- style="background: rgba(0,0,0,0) url({{asset('assets/imgs/overlays/newtv2-1.png')}}) no-repeat scroll center center / cover"
                     --}}>
 
-                    
+
 
                     <div class="col-md-6 col-sm-6" style="margin:0 auto;">
-                            <!-- Single Blog Start -->
-                            <div class="single-blog">
-                                <div class="blog-img">
-                                    <a href="#"><img class="avi-img" src="{{asset('assets/imgs/avi/avi.png')}}" alt="avi-image"></a>
-                                </div>
-
-                                <h2>Welcome</h2>
-
-                                <div class="intro-content">
-                                        <p class="mt-30">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!</p>
-                                    </div>
+                        <!-- Single Blog Start -->
+                        <div class="single-blog">
+                            <div class="blog-img">
+                                <a href="#"><img class="avi-img" src="{{asset('assets/imgs/avi/avi.png')}}" alt="avi-image"></a>
                             </div>
-                            <!-- Single Blog End -->
+
+                            <h2>Welcome</h2>
+
+                            <div class="intro-content">
+                                <p class="mt-30">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores
+                                    voluptas unde aspernatur praesentium in? Aliquam, dolore!</p>
+                            </div>
                         </div>
+                        <!-- Single Blog End -->
+                    </div>
                 </div>
 
+                <div class="intro-links">
 
+                    <ul>
+                        <li class="intro-link">
+                            <a href="#">Current Work</a>
+                        </li>
+                        <li class="intro-link">
+                            <a href="#">Github</a>
+                        </li>
+                        <li class="intro-link">
+                            <a href="#">Resume</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
