123c3f8172cda2e3ba41c5312d5b5fd095e4dfce
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 00:04:33 -0400

    Update intro.blade.php

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -7,15 +7,15 @@
     <div class="row">
 
 
-        <div class="col-md-6">
+        <div class="col-md-10">
 
 
-            <div class="intro-secton" style="background: rgba(0,0,0,0) url({{asset('assets/imgs/overlays/newtv2-1.png')}}) no-repeat scroll center center / cover">
+            <div class="intro-secton" {{-- style="background: rgba(0,0,0,0) url({{asset('assets/imgs/overlays/newtv2-1.png')}}) no-repeat scroll center center / cover" --}}>
                
                 <h1>
                     Intro
                 </h1>
-
+                <img style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
                 <div class="content clearfix">
 		
                         <header class="site clearfix">
