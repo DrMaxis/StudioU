8d3a33ca780580e286c6a05f830c612bdb85aba8
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 00:15:28 -0400

    Tv Overlay v2

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -1,60 +1,32 @@
+<div class="jumbotron vc transparent">
 
- <div class="jumbotron vc transparent">
-    
+    <div class="container">
+
+
+        <div class="row">
+
+
+            <div class="col-md-10">
+
+
+                <div class="intro-secton" {{-- style="background: rgba(0,0,0,0) url({{asset('assets/imgs/overlays/newtv2-1.png')}}) no-repeat scroll center center / cover"
+                    --}}>
+
+                    <h1>
+                        Intro
+                    </h1>
+
+
+                    <img style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
+
+                    <input type="checkbox" id="switch" checked>
+<label for="switch" class="switch-label">Turn </label>
+
 <div class="container">
-
-
-    <div class="row">
-
-
-        <div class="col-md-10">
-
-
-            <div class="intro-secton" {{-- style="background: rgba(0,0,0,0) url({{asset('assets/imgs/overlays/newtv2-1.png')}}) no-repeat scroll center center / cover" --}}>
-               
-                <h1>
-                    Intro
-                </h1>
-                <img style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
-                <div class="content clearfix">
-		
-                        <header class="site clearfix">
-                        <div class="col one">
-                            <img src="http://www.mackrichardson.com/dev/pixelspace/remote-images/logo-v.jpg" alt="591 Industries" width="740" height="729" id="logo-v" />
-                        </div>
-                            <div class="col two">
-                                <h4>591 Industries (tm) <br /> <b>H</b>euristically <b>E</b>ncrypted <b>R</b>eal-Time <b>O</b>perating <b>S</b>ystem (HEROS)</h4>
-                                <p>----------------------------------------</p>
-                                <p>HEROS v 1.0.0</p>
-                                <p>(c)2015 591 Industries</p>
-                                <p>- Server 591 -</p>
-                            </div>
-                        </header>
-                        
-                        <nav class="site clear">
-                            <ul>
-                                <li><a href="#" title="">Return Home</a></li>
-                                <li><a href="#" title="">Our Clients</a></li>
-                                <li><a href="#" title="">Contact Us</a></li>
-                            </ul>
-                        </nav>
-                                
-                        <p>System Administrator Integrated Message System (SAIMS)</p>
-                        <p>System Administrator (SYSADM) - Mack Richardson</p>
+  <iframe src="https://codepen.io" class="screen" width="800" height="600"></iframe>
+  <div class="overlay">AV-1</div>
+</div>
                 
-                        <p class="clear"><br /></p>
-                
-                        <p>Welcome to the System Administrator Integrated Message System (SAIMS). Fill out the fields below and press the SUBMIT button. The system administrator (SYSADM) will respond to your query after an appropriate amount of quiet contemplation. Thank you for contacting the System Administrator's Office.</p><br />
-                        
-                        <form>
-                            <label>Name >></label><input type="text" /><br />
-                            <label>Email >></label><input type="text" /><br />
-                            <label>Subject >></label><input type="text" /><br />
-                            <label>Message >></label><textarea id="text" rows="1"></textarea><br /><br /><br />
-                            <input type="submit" value="Submit" />
-                            <a class="button" alt="" href="index.html">Cancel</a>
-                        </form>
-                    </div>
                 </div>
 
 
