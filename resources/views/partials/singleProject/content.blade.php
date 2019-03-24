7f8327274fe6dba331b9d94d5ad10cd5469700ce
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 13:08:53 -0400

    Update content.blade.php

--- resources/views/partials/singleProject/content.blade.php
+++ resources/views/partials/singleProject/content.blade.php
@@ -60,79 +60,88 @@
                 </div>
             </div>
         </div>
-      {{--   <!-- Row End --> --}}
-    </div>
-   {{--  <!-- Container End --> --}}
+      {{--   <!-- Row End -->--}}
+</div>
+{{--
+<!-- Container End -->--}}
 </div>-->
 
 <div class="project-content-area ptb-100 vc mt-40">
-            <div class="container">
-                <div class="row">
+    <div class="container">
+        <div class="row">
 
-<div class="col-md-8 col-sm-12 m0-a">
-                        <!-- Blog Details Start -->
-                        <div class="project-details">
-                            <div class="project-img">
-                                <img src="{{asset('assets/imgs/dummy/5.jpg')}}" alt="blog-image">
-                            </div>
-                            
-                            <h3 class="semi-title">Lorem ipsum dolor sit amet</h3>
-                            <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example,</p>
-                            <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
-                            <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
-                            <div class="project-img project-single-img">
-                                <div class="row">
-                                    <div class="col-sm-6 mt-30">
-                                        <img src="{{asset('assets/imgs/dummy/10.jpg')}}" alt="blog-image">
-                                    </div>
-                                    <div class="col-sm-6 mt-30">
-                                        <img src="{{asset('assets/imgs/dummy/10.jpg')}}" alt="blog-image">
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
-                            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
-                            <!--  Blog-Share Start  -->
-                            <div class="project-share mtb-50">
-                                <div class="row">
-                                    <div class="col-lg-4 col-md-5 col-sm-6">
-                                        <span class="pull-left">CATEGORYS:</span>
-                                        <ul class="list-inline">
-                                            <li><a href="#">Photography</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-lg-8 col-md-7 col-sm-6">
-                                        <div class="social-links text-right">
-                                            <ul class="social-link-list list-inline">
-                                                <li>Share:</li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-reddit"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- End of Row -->
-                            </div>
-                            <!--  Blog-Share End  -->
-                            <!--  Blog-Pager Start  -->
-                            
-                            <!--  Blog-Pager End  -->
-                        </div>
-                        <!-- Blog Details End -->
-                        <!-- Blog Realated Post Start -->
-                        
-                        <!-- Blog Realated Post Start -->
-                        <!-- Comment Area Start -->
-                        
-                        <!-- Comment Area End -->
-                        <!-- Contact Email Area Start -->
-                        
-                        <!-- Contact Email Area End -->
+            <div class="col-md-8 col-sm-12 m0-a">
+                <!-- Blog Details Start -->
+                <div class="project-details">
+                    <div class="project-img">
+                        <img src="{{asset('assets/imgs/dummy/5.jpg')}}" alt="blog-image">
                     </div>
+
+                    <h3 class="semi-title">Lorem ipsum dolor sit amet</h3>
+                    <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+                        and I will give you a complete account of the system, and expound the actual teachings of the great
+                        explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids
+                        pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure
+                        rationally encounter consequences that are extremely painful. Nor again is there anyone who loves
+                        or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances
+                        occur in which toil and pain can procure him some great pleasure. To take a trivial example,</p>
+                    <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+                        and I will give you a complete account of the system, and expound the actual teachings of the great
+                        explorer of the truth, the master-builder of human happiness.</p>
+                    <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+                        and I will give you a complete account of the system, and expound the actual teachings of the great
+                        explorer of the truth, the master-builder of human happiness.</p>
+                    <div class="project-img project-single-img">
+                        <div class="row">
+                            <div class="col-sm-6 mt-30">
+                                <img src="{{asset('assets/imgs/dummy/10.jpg')}}" alt="blog-image">
+                            </div>
+                            <div class="col-sm-6 mt-30">
+                                <img src="{{asset('assets/imgs/dummy/10.jpg')}}" alt="blog-image">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+                        and I will give you a complete account of the system, and expound the actual teachings of the great
+                        explorer of the truth, the master-builder of human happiness.</p>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+                        and I will give you a complete account of the system, and expound the actual teachings of the great
+                        explorer of the truth, the master-builder of human happiness.</p>
+                    <!--  Blog-Share Start  -->
+                    <div class="project-share mtb-50">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-5 col-sm-6">
+                                <span class="pull-left">Live Site</span>
+                                <ul class="list-inline">
+                                    <li><a href="#"></a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-lg-4 col-md-5 col-sm-6">
+                                <span class="pull-right">GitHub Project</span>
+                                <ul class="list-inline">
+                                    <li><a href="#"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- End of Row -->
+                    </div>
+                    <!--  Blog-Share End  -->
+                    <!--  Blog-Pager Start  -->
+
+                    <!--  Blog-Pager End  -->
                 </div>
+                <!-- Blog Details End -->
+                <!-- Blog Realated Post Start -->
+
+                <!-- Blog Realated Post Start -->
+                <!-- Comment Area Start -->
+
+                <!-- Comment Area End -->
+                <!-- Contact Email Area Start -->
+
+                <!-- Contact Email Area End -->
             </div>
+        </div>
+    </div>
 </div>
