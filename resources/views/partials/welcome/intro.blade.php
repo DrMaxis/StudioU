e275134817eea0bd69d861066ca62ed5833955b1
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 02:28:02 -0400

    Intro Content

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -17,22 +17,18 @@
                     </h1>
 
 
-                    <div class="col-md-4 col-sm-6">
+                    <div class="col-md-6 col-sm-6" style="margin:0 auto;">
                             <!-- Single Blog Start -->
                             <div class="single-blog">
                                 <div class="blog-img">
                                     <a href="blog-details.html"><img src="{{asset('assets/imgs/avi/face.jpg')}}" alt="blog-image"></a>
                                 </div>
-                                <div class="blog-content">
-                                    <div class="blog-content-upper">
-                                        <h6 class="blog-title"><a href="blog-details.html">Coco Lapine Design</a></h6>
-                                        <p>Interior designer Amber Lewis’s blog takes you inside the creative workings of her Los Angeles–based studio.</p>
+
+                                <h2>Welcome</h2>
+
+                                <div class="intro-content">
+                                        <p class="mt-30">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!</p>
                                     </div>
-                                    <div class="blog-content-lower">
-                                        <a href="">Nevara</a>
-                                        <span class="f-right">05 Nov, 2018</span>
-                                    </div>
-                                </div>
                             </div>
                             <!-- Single Blog End -->
                         </div>
