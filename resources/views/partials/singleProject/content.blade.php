d3e3a23eb7b92586d604ac8a36b3020f142058f0
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 12:46:00 -0400

    Update content.blade.php

--- resources/views/partials/singleProject/content.blade.php
+++ resources/views/partials/singleProject/content.blade.php
@@ -105,12 +105,12 @@
                                         <div class="social-links text-right">
                                             <ul class="social-link-list list-inline">
                                                 <li>Share:</li>
-                                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-reddit"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                                <li class="list-inline-item" ><a href="#"><i class="fa fa-reddit"></i></a></li>
                                             </ul>
                                         </div>
                                     </div>
