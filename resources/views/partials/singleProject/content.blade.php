97c1fa17287ea16b8979745187a75494e8f76b23
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 13:06:35 -0400

    .

--- resources/views/partials/singleProject/content.blade.php
+++ resources/views/partials/singleProject/content.blade.php
@@ -72,7 +72,7 @@
 <div class="col-md-8 col-sm-12 m0-a">
                         <!-- Blog Details Start -->
                         <div class="project-details">
-                            <div class="blog-img">
+                            <div class="project-img">
                                 <img src="{{asset('assets/imgs/dummy/5.jpg')}}" alt="blog-image">
                             </div>
                             
@@ -80,7 +80,7 @@
                             <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example,</p>
                             <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
                             <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
-                            <div class="blog-img blog-single-img">
+                            <div class="project-img project-single-img">
                                 <div class="row">
                                     <div class="col-sm-6 mt-30">
                                         <img src="{{asset('assets/imgs/dummy/10.jpg')}}" alt="blog-image">
@@ -93,7 +93,7 @@
                             <p class="mb-20">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
                             <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
                             <!--  Blog-Share Start  -->
-                            <div class="blog-share mtb-50">
+                            <div class="project-share mtb-50">
                                 <div class="row">
                                     <div class="col-lg-4 col-md-5 col-sm-6">
                                         <span class="pull-left">CATEGORYS:</span>
