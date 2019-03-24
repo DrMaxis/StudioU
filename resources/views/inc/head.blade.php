928f9c62257a7859a67ddd074a67c54fe006c110
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-23 23:04:20 -0400

    Update head.blade.php

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -72,9 +72,9 @@
       </filter>
     </svg>
   
-  
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
+
   
     <!--/-/-/-/-/-/-/-/-/ 
   
