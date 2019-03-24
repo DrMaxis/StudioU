0cf816fcc2b3085c3e17d92b6b07268097032e51
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-24 02:10:37 -0400

    add avi

--- resources/views/partials/welcome/intro.blade.php
+++ resources/views/partials/welcome/intro.blade.php
@@ -17,7 +17,7 @@
                     </h1>
 
 
-                    <img class="overlay-img" style="width:100%;" src="{{asset('assets/imgs/overlays/newtv2-1.png')}}" alt="">
+                    <img class="overlay-img" style="width:100%;" src="{{asset('assets/imgs/avi/avi.png')}}" alt="">
                 </div>
 
 
